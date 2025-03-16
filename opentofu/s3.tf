8e36693e1c94cbd8bd4244589a7e244888a1eff3
Paolo Flores <pflores.fisi22@gmail.com>
2025-03-15 22:56:41 -0500

    deploy s3 bucket

---
+++ opentofu/s3.tf
@@ -0,0 +1,38 @@
+resource "aws_s3_bucket" "lambdirs" {
+  bucket        = "${local.env}-${var.bucket_prefix}lambdirs"
+  force_destroy = true
+
+  tags = {
+    Name        = "${local.env}-lambdirs"
+    Environment = local.env
+  }
+}
+
+data "aws_iam_policy_document" "lambdirs_bucket_policy_doc" {
+  statement {
+    effect = "Deny"
+
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    actions = ["s3:*"]
+
+    resources = [
+      aws_s3_bucket.lambdirs.arn,
+      "${aws_s3_bucket.lambdirs.arn}/*",
+    ]
+
+    condition {
+      test     = "Bool"
+      variable = "aws:SecureTransport"
+      values   = ["false"]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "lambdirs" {
+  bucket = aws_s3_bucket.lambdirs.id
+  policy = data.aws_iam_policy_document.lambdirs_bucket_policy_doc.json
+}
