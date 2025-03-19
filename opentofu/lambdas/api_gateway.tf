fe98007ecc5eb5b1d9e0669884b1fe3b0659aa89
Paolo Flores <pflores.fisi22@gmail.com>
2025-03-18 21:46:28 -0500

    inline api gateway role and policies

--- opentofu/lambdas/api_gateway.tf
+++ opentofu/lambdas/api_gateway.tf
@@ -76,6 +76,58 @@ resource "aws_cloudwatch_log_group" "lambdirs_gw" {
   }
 }
 
+/* APi Gateway role and policies */
+
+resource "aws_api_gateway_account" "apigw_account" {
+  cloudwatch_role_arn = aws_iam_role.lambdirs.arn
+}
+
+resource "aws_iam_role" "lambdirs" {
+  name = "APIGatewayCloudWatchRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "apigateway.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "lambdirs_policy" {
+  name        = "APIGatewayLoggingPolicy"
+  description = "Allows API Gateway to write logs to CloudWatch"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:DescribeLogGroups",
+          "logs:DescribeLogStreams",
+          "logs:PutLogEvents",
+          "logs:GetLogEvents",
+          "logs:FilterLogEvents"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambdirs" {
+  policy_arn = aws_iam_policy.lambdirs_policy.arn
+  role       = aws_iam_role.lambdirs.name
+}
+
 /* Outputs */
 
 output "api_gateway_url" {
